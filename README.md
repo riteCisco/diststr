efcc4abe5e9f325c77d1900ed1e8e7d933e2aa0d
l <bryhuang@cisco.com>
2024-05-25 14:04:47 -0500

    prototype v1.3 (unoptimized, no logging)

--- README.md
+++ README.md
@@ -10,19 +10,23 @@
 3. To run (starting from diststr directory):
 4.     cd src
 5.     cargo run -- {domain name} {path to wordlist} {Other flags THERE BUT NOT NO FUNCTIONALITY IMPLEMENTED YET}
-# FLAG INSTRUCTION ***NOT IMPLEMENTED (mainly cause I don't know if it's completely necessary or not)***
-- -l -> Chance if being leetspoken
-- -c -> Chance of being found if testing upper/lowercase variation
-- -64 -> Compared against Best64 rule list
-- -d -> Compared against dive rule list
-
+# FLAG INSTRUCTION
+### These flags might add a significant amount of time to the process
+- -l -> Add on a list of leetcode variations to test
+- -c -> Add on a list of upper/lowercase variations to test
+- -64 -> Add on a list of best64 rules to test
+- -di -> Add on a list of dive rules to test
+### These flags might add unnecessary clutter to the console
+- -d -> Prints out debug messages and shows whenever there is a new biggest and shortest distance
 # Rationale
 - The Damerau–Levenshtein distance allows the transposition of two adjacent characters alongside insertion, deletion, substitution
     - See theory here: https://en.wikipedia.org/wiki/Damerau–Levenshtein_distance
+    - There will be a VERY small weight placed on transposition, as even though I want to be able to account for that, it is more of a "if something is mistyped or if there is a human error" type change.
 - Thus, it's possible to tell how DIFFERENT something is from a certain entry, and be able to extrapolate how many extra "rules" might need to be added in order to get there
 - I was thinking to add some of the funny rule stuff to minimize keyspace, but the thought process is that if I'm only measuring the DISTANCE it might not be necessary to try to counter the PERMUTATIONS specifically
     - Waiting for more guidance on that
 - I treated this a lot like a password complexity assessment, with more emphasis on weighted averages on maybe what changes need to be made?
+- New idea: Simpler... instead of weighting the averages, offer to create new list to sort through, it's much simpler. 
 
 # Guidance Requested
 - The permutation and rule stuff. I don't know if I'm overthinking it at all
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::io::BufRead;
 use std::path::Path;
 use colored::Colorize;
-
+use std::collections::HashMap;
 //use std::io::prelude::*;
 //use cewl to gen wordlist
 //Variations based off of hashcat (one rule to rule them all)
@@ -14,12 +14,104 @@ that allows for different length
 strings, and (with Damerau) 
 for transpositions
  */
+/*fn sixfourify(&str) -> Vec<String> {
 
+}*/
+
+fn l33tify(word: &str) -> Vec<String> {
+    // Define leetspeak substitutions (can be expanded with more substitutions)
+    let subs: HashMap<char, Vec<char>> = [
+        ('a', vec!['4', '@']),
+        ('e', vec!['3']),
+        ('i', vec!['1', '!']),
+        ('o', vec!['0']),
+        ('s', vec!['$', '5']),
+        ('t', vec!['7']),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    let mut combinations = Vec::new();
+    let word_chars: Vec<char> = word.chars().collect();
+    genrec(&subs, &word_chars, 0, &mut String::new(), &mut combinations);
+    
+    combinations
+}
+
+fn caseify(input: &str) -> Vec<String> {
+    let mut permutations = Vec::new();
+    genperm(input.chars().collect(), 0, &mut String::new(), &mut permutations);
+    permutations
+}
+
+fn genperm(chars: Vec<char>, index: usize, current: &mut String, permutations: &mut Vec<String>) {
+    if index == chars.len() {
+        permutations.push(current.clone());
+        return;
+    }
+
+    // Add the lowercase version of the current character and recurse
+    current.push(chars[index].to_ascii_lowercase());
+    genperm(chars.clone(), index + 1, current, permutations);
+    current.pop();
+
+    // Add the uppercase version of the current character and recurse
+    current.push(chars[index].to_ascii_uppercase());
+    genperm(chars, index + 1, current, permutations);
+    current.pop();
+}
+
+fn genrec(
+    subs: &HashMap<char, Vec<char>>,
+    word: &[char],
+    index: usize,
+    current: &mut String,
+    combinations: &mut Vec<String>,
+) {
+    if index == word.len() {
+        combinations.push(current.clone());
+        return;
+    }
+
+    let ch = word[index];
+    // Always include the original character
+    current.push(ch);
+    genrec(subs, word, index + 1, current, combinations);
+    current.pop();
+
+    // Include leetspeak substitutions if they exist
+    if let Some(sub_chars) = subs.get(&ch) {
+        for &sub_ch in sub_chars {
+            current.push(sub_ch);
+            genrec(subs, word, index + 1, current, combinations);
+            current.pop();
+        }
+    }
+}
  fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
  where P: AsRef<Path>, {
      let file = File::open(filename)?;
      Ok(io::BufReader::new(file).lines())
  }
+
+fn expandomatic(str: &str, flags: (bool, bool, bool, bool, bool)) -> Vec<String>{
+    let mut master_vec: Vec<String> = Vec::new(); 
+    master_vec.push(str.to_string());
+    if flags.1 { //leet
+        master_vec.extend(l33tify(str)); 
+    }
+    if flags.2 {
+        master_vec.extend(caseify(str));
+    }
+
+    master_vec
+    /*let mut file = File::create("./genfile").expect("Error creating file");
+
+    for line in masterVec {
+        writeln!(file, "{}", line).expect("Failure to write to file");
+    }*/
+}
 
 fn dldist(s1: &str, s2: &str) -> usize {
     let s1_chars: Vec<_> = s1.chars().collect();
@@ -40,10 +132,11 @@ fn dldist(s1: &str, s2: &str) -> usize {
     for i in 1..=s1_len {
         for j in 1..=s2_len {
             let cost = if s1_chars[i - 1] == s2_chars[j - 1] { 0 } else { 1 };
+
             distances[i][j] = *[
-                distances[i - 1][j] + 1,    // deletion
-                distances[i][j - 1] + 1,    // insertion
-                distances[i - 1][j - 1] + cost,  // substitution
+                distances[i - 1][j] + 1, // deletion
+                distances[i][j - 1] + 1, // insertion
+                distances[i - 1][j - 1] + cost, // substitution
             ]
             .iter()
             .min()
@@ -56,7 +149,7 @@ fn dldist(s1: &str, s2: &str) -> usize {
             {
                 distances[i][j] = std::cmp::min(
                     distances[i][j],
-                    distances[i - 2][j - 2] + cost,
+                    distances[i - 2][j - 2] + 2, // transposition with higher cost
                 );
             }
         }
@@ -64,62 +157,77 @@ fn dldist(s1: &str, s2: &str) -> usize {
 
     distances[s1_len][s2_len]
 }
+
 fn main() {
-    let _args1: Vec<String> = env::args().collect();
-    dbg!(&_args1);
-    let mut _args: Vec<String> = _args1;
-    let mut argsze = _args.len();
+    let args1: Vec<String> = env::args().collect();
+    dbg!(&args1);
+    let mut args: Vec<String> = args1;
+    let mut argsze = args.len();
     let entry;
     let filename;
     let mut smallest_dist = 10000;
     let mut biggest_dist= 0;
     let mut smallest_word = String::new();
     let mut list_flag = (false, false, false, false, false);
-    entry = _args.get(1).unwrap().to_string();
-    _args.remove(1);
+    entry = args.get(1).unwrap().to_string();
+    args.remove(1);
     argsze -= 1;
     println!("Entry: {entry}");
-    filename = _args.get(1).unwrap().to_string();
-    _args.remove(1);
+    filename = args.get(1).unwrap().to_string();
+    args.remove(1);
     argsze = argsze - 1;
     println!("List Path: {filename}");
     argsze -= 1;
     while argsze != 0 {
-        if  _args.get(1).unwrap().to_string() == "-d" {
+        let ph = args.get(1).unwrap();
+        if  ph == "-d" {
             list_flag.0 = true;
-            _args.remove(1);
+            args.remove(1);
             argsze -= 1;
             continue;
         } 
-        if  _args.get(1).unwrap() == "-l" { //leet
-            println!("!");
+        if  ph == "-l" { //leet
+            //println!("!");
+            list_flag.1 = true;
             //here would be the inclusion of a weighted average of the highest DL Distance and the possibility of discovery based off of leetspeak changes
-            _args.remove(1);
+            args.remove(1);
             argsze -= 1;
             continue;
         } 
-        if  _args.get(1).unwrap() == "-c" { //caps
-            println!("?");
+        if  ph == "-c" { //caps
+            //println!("?");
+            list_flag.2 = true;
             //here would be the inclusion of a weighted average including the usage of lower/uppercase, as the entire thing is case sensitive
             //TO-DO: Figure out if this is needed
-            _args.remove(1);
+            args.remove(1);
             argsze -= 1;
             continue;
         }
-        if  _args.get(1).unwrap() == "-64" { //stacked best64 
-            println!("@");
+        if  ph == "-64" { //stacked best64 
+            //println!("@");
+            list_flag.3 = true;
             //stacked once best64 check (not a lot of rules, should be able to brute force)
-            _args.remove(1); 
+            args.remove(1); 
             argsze -= 1;
             continue;
         }
-        if  _args.get(1).unwrap() == "-d" { //dive.rules
+        if  ph == "-di" { //dive.rules
             println!("why");
+            list_flag.4 = true;
             //why. there's no true pattern, so this might also need to be brute forced
-            _args.remove(1);
+            args.remove(1);
             argsze -= 1;
             continue;
         }
+        eprintln!("Invalid flag {}", ph);
+        std::process::exit(1);
+    }
+    if list_flag.0 {
+        println!("Debug flag set to {}", list_flag.0);
+        println!("Leet flag set to {}", list_flag.1); 
+        println!("Upper/Lower flag set to {}", list_flag.2);
+        println!("64 flag set to {}", list_flag.3);
+        println!("Dive flag set to {}", list_flag.4);
     }
     //there's always space to add more, but these are the four "cool" ones. everything is possible imo except for dive, that looks like
     //it could be a real pain 
@@ -127,32 +235,33 @@ fn main() {
     //file.read_line(&mut ph);
     //println!("{entry}");
     let mut flag = true;
-    let mut result;
     if let Ok(lines) = read_lines(filename) {
         // Consumes the iterator, returns an (Optional) String
         for line in lines.flatten() {
-            result = line.to_string();
-            let newdist = dldist(&result, &entry);
-            //println!("{newdist}");
-            if newdist < smallest_dist {
-                smallest_dist = newdist; //NEEDS A TIE BREAKER THAT DOES NOT ADD A BUNCH OF COMPLEXITY
-                smallest_word = result.clone();
-                if flag {
-                    flag = false;
-                    continue;
+            let test_list = expandomatic(&line, list_flag);
+            for test in test_list {
+                let newdist = dldist(&test, &entry);
+                //println!("{newdist}");
+                if newdist < smallest_dist {
+                    smallest_dist = newdist; //NEEDS A TIE BREAKER THAT DOES NOT ADD A BUNCH OF COMPLEXITY
+                    smallest_word = test.clone();
+                    if flag {
+                        flag = false;
+                        continue;
+                    }
+                    if list_flag.0 {
+                        println!("New Winner! {} + {} with {}", test.blue(), entry.blue(), newdist.to_string().blue());
+                    }
                 }
-                if list_flag.0 {
-                    println!("New Winner! {} + {} with {}", result.blue(), entry.blue(), newdist.to_string().blue());
-                }
-            }
-            if newdist > biggest_dist {
-                biggest_dist = newdist;
-                if flag {
-                    flag = false;
-                    continue;
-                }
-                if list_flag.0 {
-                println!("New Loser! {} + {} with {}", result.red(), entry.red(), newdist.to_string().red());
+                if newdist > biggest_dist {
+                    biggest_dist = newdist;
+                    if flag {
+                        flag = false;
+                        continue;
+                    }
+                    if list_flag.0 {
+                        println!("New Loser! {} + {} with {}", test.red(), entry.red(), newdist.to_string().red());
+                    }
                 }
             }
         }
@@ -160,7 +269,7 @@ fn main() {
     println!("Base Answer: {}, with {}", smallest_word.green(), smallest_dist.to_string().green());
     //println!("Damerau-Levenshtein distance: {}", dldist(s1, s2));
     //the challenge isn't the distance itself, the addition of how many changes it would take is what I want
-    //println!("{}", _args.get(1).unwrap());
+    //println!("{}", args.get(1).unwrap());
     //Levenstein is a HUGE keyspace. Assume naive distance
     //The bigger the distance is, the better. This assumes that  
     
